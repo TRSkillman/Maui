1145a450dcd89e474ea283b421bd9cd49a26b8ca
Tim Skillman <tskillman@silhouettesoftware.com>
2022-10-04 15:02:38 +0100

    Fix for upgrading to SkiaSharp 2.88.3.  See SkiaCanvasSetup.csproj file

--- SkiaCanvasSetup/SkiaCanvasSetup/MainPage.xaml.cs
+++ SkiaCanvasSetup/SkiaCanvasSetup/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SkiaCanvasSetup;
+﻿using SkiaSharp;
+
+namespace SkiaCanvasSetup;
 
 public partial class MainPage : ContentPage
 {
@@ -9,12 +11,16 @@ public partial class MainPage : ContentPage
 
 	private void OnPaintSurface(object sender, SkiaSharp.Views.Maui.SKPaintSurfaceEventArgs e)
 	{
+		SKCanvas canvas = e.Surface.Canvas;
+		SKPaint paint = new SKPaint { Color = SKColors.DarkBlue, Style = SKPaintStyle.Stroke, IsAntialias = true };
 
-    }
+		canvas.Clear();
+		canvas.DrawOval(new SKRect(200,200,100,100), paint);
+	}
 
 	private void OnTouch(object sender, SkiaSharp.Views.Maui.SKTouchEventArgs e)
 	{
 
-    }
+	}
 }
 
